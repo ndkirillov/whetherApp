9eb8fc117a72f7e7e2a7d05c13cf0e8eaf802557
ndkirillov <nikitch0905@gmail.com>
2021-06-04 17:32:18 +0500

    test2

--- app/src/androidTest/java/org/hse/android/whetherapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/hse/android/whetherapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.hse.android.whetherapp", appContext.getPackageName()); //test
+        assertEquals("org.hse.android.whetherapp", appContext.getPackageName());
     }
 }
